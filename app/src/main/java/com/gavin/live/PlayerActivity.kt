5687cf1d5637fba7b19802c2d27f6d79294394b5
songweiliang <weiliang.song@starscape.live>
2021-01-27 17:32:41 +0800

    [ADD]ExoPlayer

--- app/src/main/java/com/gavin/live/PlayerActivity.kt
+++ app/src/main/java/com/gavin/live/PlayerActivity.kt
@@ -1,12 +1,20 @@
 package com.gavin.live
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 import kotlinx.android.synthetic.main.activity_player_layout.*
 
+
 class PlayerActivity : AppCompatActivity() {
+
+    private lateinit var player:SimpleExoPlayer;
+    private val DEFAULT_MEDIA_URI =
+        "https://storage.googleapis.com/exoplayer-test-media-1/mkv/android-screens-lavf-56.36.100-aac-avc-main-1280x720.mkv"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_layout)
@@ -26,5 +34,19 @@ class PlayerActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show();
         }
+
+        player = SimpleExoPlayer.Builder(this).build()
+        PlayerVideoView.player = player;
+
+
+        val mediaItem: MediaItem = MediaItem.fromUri(Uri.parse(DEFAULT_MEDIA_URI))
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    override fun onDestroy() {
+        player.release()
+        super.onDestroy()
     }
 }
